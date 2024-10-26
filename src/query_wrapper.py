d1b00d99707e163fd13feebed09be61aeb424cf5
Minho Lee <lmh30002@gmail.com>
2024-10-26 16:30:49 +0900

    works for default widgets

--- src/query_wrapper.py
+++ src/query_wrapper.py
@@ -4,76 +4,103 @@ import hashlib
 from typing import Callable, Any
 from functools import wraps, partial
 import inspect
-__all__ = ["checkbox", "radio", "selectbox", "multiselect", "slider", "text_input", "number_input"]
+__all__ = ["checkbox", "radio", "selectbox", "multiselect", "slider", "text_input", "number_input", "sharable_link"]
 
 def SHA1(obj):
     return hashlib.sha1(str(obj).encode()).hexdigest()
 
-# 1) QueryWrapper(st.xxx("asdf", ...), query="") -> 불가능
-# 2) QueryWrapper(st.xxx, )
-# 3) QueryWrapper(st.xxx)(xxx)
-# 4) stq.radio(xxx) -> 제일 무난한듯?
-
 _QUERY_TABLES = {}
 
-def load_value_from_query(query: str):
+def sharable_link():
+    result = ""
+    for key, value in _QUERY_TABLES.items():
+        if isinstance(value, list):
+            for item in value:
+                result += f"{key}={item}&"
+        else:
+            result += f"{key}={value}&"
+    return result.strip("&")
+
+def map_positional_arg_to_keyword_args(module, *args):
+    signature = inspect.signature(module)
+    params = list(signature.parameters.keys())
+    return {params[i]: args[i] for i in range(len(args))}
+
+def load_value_from_query(query: str, hash_table: dict[str, Any]):
     values = st.query_params.get_all(query)
     if query in st.query_params: st.query_params.pop(query)
-    return values
+    return [hash_table.get(value, value) for value in values]
 
 @st.cache_data
 def load_hash_table(items: list[Any]):
-    return {SHA1(item): item for item in items}
+    return {SHA1(item)[:3]: item for item in items if should_use_hash(item)}
 
 def should_use_hash(value: Any):
-    return isinstance(value, str) and len(value) > 8
+    return isinstance(value, str) and (len(value) > 8 or len(value) == 0 or any(x in value for x in "?&=/"))
 
-def load_options(widget, *args, **kwargs):
-    match widget:
-        case st.radio:
-            return args[1] if len(args) > 1 else kwargs["options"],
-        case st.selectbox:
-            return args[1] if len(args) > 1 else kwargs["options"],
-        case st.multiselect:
-            return args[1] if len(args) > 1 else kwargs["options"],
-
-def set_default_value(widget, value: list[str]):
-    key = None
+def translate_default_value(widget, value: list[str], options: list[Any] | None = None):
     match widget:
         case st.checkbox:
             key = "value"
-            value = eval(value[0])
+            _value = eval(value[0])
         case st.radio:
             key = "index"
+            _value = options.index(value[0]) # type: ignore
         case st.selectbox:
-            key = "value"
+            key = "index"
+            _value = options.index(value[0]) # type: ignore
         case st.multiselect:
             key = "default"
+            _value = value
         case st.slider:
             key = "value"
+            _value = int(value[0])
         case st.text_input:
             key = "value"
+            _value = value[0]
         case st.number_input:
             key = "value"
+            _value = eval(value[0])
+        case _:
+            return {}
+    return {key: _value}
 
 
-def wrap(widget, *, query: str | None = None, option_key: str | None = None, default_key: str | None = None):
-    key = inspect.signature(widget).parameters.get("key")
-    @wraps(widget.__call__)
-    def inner_widget(*args, **kwargs):
+
+def wrap(widget: Callable):
+    @wraps(widget)
+    def inner_widget(*args, query: str | None = None, **kwargs):
+        global _QUERY_TABLES
+        mapped_kwargs = map_positional_arg_to_keyword_args(widget, *args) | kwargs
+        _query = query or mapped_kwargs.get("key", mapped_kwargs.get("label"))
         
-        items = load_options(widget, *args, **kwargs)
-        default_value = load_value_from_query(query)
-        hash_table = load_hash_table(items)
+        options = mapped_kwargs.get("options", None)
+        hash_table = {}
+        reverse_hash_table = {}
+        if options is not None:
+            hash_table = load_hash_table(options)
+            reverse_hash_table = {value: key for key, value in hash_table.items()}
+        print(_query, hash_table, options)
 
-        return widget(*args, **kwargs)
+        default_value = load_value_from_query(_query, hash_table)
+        if default_value:
+            mapped = translate_default_value(widget, default_value, options)
+            mapped_kwargs |= mapped
+
+        result = widget(**mapped_kwargs)
+        query_value = [reverse_hash_table.get(value, value) for value in result] if isinstance(result, list) else reverse_hash_table.get(result, result)
+        if options is not None:
+            _QUERY_TABLES[_query] = query_value
+        else:
+            _QUERY_TABLES[_query] = result
+        return result
     return inner_widget
 
-checkbox = partial(wrap, st.checkbox)
-radio = partial(wrap, st.radio)
-selectbox = partial(wrap, st.selectbox)
-multiselect = partial(wrap, st.multiselect)
-slider = partial(wrap, st.slider)
-text_input = partial(wrap, st.text_input)
-number_input = partial(wrap, st.number_input)
+checkbox = wrap(st.checkbox)
+radio = wrap(st.radio)
+selectbox = wrap(st.selectbox)
+multiselect = wrap(st.multiselect)
+slider = wrap(st.slider)
+text_input = wrap(st.text_input)
+number_input = wrap(st.number_input)
 
